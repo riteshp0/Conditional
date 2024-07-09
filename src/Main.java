0490050919ac81d7742d016051836f4dde4eaacc
Dell <riteshpaudel529@gmail.com>
2024-07-09 13:57:38 +0545

    rorwngo

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int num1=44;
+        int num2=26;
+        int num3=99;
+
+        if (num1>num2 && num1>num3) {
+            System.out.println(num1+ " "+"is the greatest among the given numbers.");
+        }
+        else if (num2>num1 && num2>num3) {
+            System.out.println(num2+" "+"is the greatest among the given numbers.");
+        }
+        else if (num3>num1 && num3>num2) {
+            System.out.println(num3+" "+"is the greatest among the given numbers.");
+        }
+
+    }
+}
