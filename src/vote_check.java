60402a44f1794b0893c0e3b065034b1340cc0b7e
Dell <riteshpaudel529@gmail.com>
2024-07-09 14:19:20 +0545

    rorwngo

---
+++ src/vote_check.java
@@ -0,0 +1,18 @@
+public class vote_check {
+    public static void main(String[] args) {
+
+        int age = 19;
+        String citizenship ="Nepal";
+
+        if (age >= 18 && citizenship == "Nepal") {
+            System.out.println("You are allowed to vote");
+        }
+        else  {
+            System.out.println("You are not allowed to vote");
+        }
+
+
+    }
+}
+
+
